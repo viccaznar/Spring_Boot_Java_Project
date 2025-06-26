353c000547373d6e34289a74e0079ba0b3fef658
Marcos <viccaznar@yahoo.com>
2025-06-26 16:48:09 -0300

    Project, User and UserResource Created
    Project created
    
    Checklist:
    - Spring Initializr
    - Maven
    - Java 17
    - Packing JAR
    - Dependencies: Spring Web
    
    User entity and resource
    
    Basic entity checklist:
    - Basic attributes
    - Associations (instantiate collections)
    - Constructors
    - Getters & Setters (collections: only get)
    - hashCode & equals
    - Serializable

---
+++ sbproject/sbproject/src/main/java/io/aznar/sbproject/SbprojectApplication.java
@@ -0,0 +1,13 @@
+package io.aznar.sbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbprojectApplication.class, args);
+	}
+
+}
---
+++ sbproject/sbproject/src/test/java/io/aznar/sbproject/SbprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aznar.sbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
