04aec60023bded49904d3de8ca1b2dcefbe35315
Marcos <viccaznar@yahoo.com>
2025-06-27 07:15:32 -0300

    Service layer, component registration

---
+++ sbproject/sbproject/src/main/java/io/aznar/sbproject/services/UserService.java
@@ -0,0 +1,26 @@
+package io.aznar.sbproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.aznar.sbproject.entities.User;
+import io.aznar.sbproject.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
